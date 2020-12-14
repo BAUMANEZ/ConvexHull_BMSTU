8f21772f7c28f7925ede97734e6181e6dc53fc1a
Arseny <arseny.tokarev@gmail.com>
2020-12-14 19:11:19 +0300

    Initialized KPS

---
+++ Kirkpatrick-Seidel/Kirkpatrick-Seidel/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Kirkpatrick-Seidel
+//
+//  Created by Арсений Токарев on 14.12.2020.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+	// insert code here...
+	std::cout << "Hello, World!\n";
+	return 0;
+}
